bdabffafb689488d35e8caf466775f68ddad9cb8
Natalia Veras <verasnatali4@gmail.com>
2024-06-16 13:24:14 +0200

    [Auth] State, reducers and selectors created

--- src/app/auth/components/poke-login.component.ts
+++ src/app/auth/components/poke-login.component.ts
@@ -5,6 +5,10 @@ import { MatSelectModule } from '@angular/material/select';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatButtonModule } from '@angular/material/button';
+import { Store } from '@ngrx/store';
+import { PokeState } from '../../store/app.state';
+import { login } from '../store/auth.actions';
+import { selectLoggedInStatus } from '../store/auth.selectors';
 
 @Component({
   selector: 'poke-login',
@@ -27,9 +31,28 @@ export class PokeLoginComponent {
     password: '',
   });
 
-  constructor(private formBuilder: FormBuilder) {}
+  constructor(
+    private formBuilder: FormBuilder,
+    private store: Store<PokeState>
+  ) {}
 
   onSubmit() {
+    // Validate form
+    // Manage form errors
+    // Get user's information
+    // Generate random token
+    // Dispatch user's information and token to store
+    const user = {
+      email: this.loginForm.value.email,
+      password: this.loginForm.value.password,
+    };
+    // Dispatch isLoggedIn to store
+    // If valid
+    this.store.dispatch(login({ request: { isLoggedIn: true } }));
+    // Navigate to dashboard
     console.log('this.loginForm', this.loginForm.value);
+    this.store.select(selectLoggedInStatus).subscribe((isLoggedIn) => {
+      console.log('isLoggedIn', isLoggedIn);
+    });
   }
 }
