5cad54beea20284072c028f168703a7e81ec9264
Natalia Veras <verasnatali4@gmail.com>
2024-06-17 21:47:34 +0200

    [Dashboard] Installed chart.js, added charts with no information

--- src/app/dashboard/components/poke-dashboard.component.ts
+++ src/app/dashboard/components/poke-dashboard.component.ts
@@ -5,11 +5,23 @@ import { selectUser } from '../../auth/store/auth.selectors';
 import { CommonModule } from '@angular/common';
 import { Observable } from 'rxjs';
 import { User } from '../../auth/types/user.interface';
+import { DockModule } from 'primeng/dock';
+import { FormsModule } from '@angular/forms';
+import { RadioButtonModule } from 'primeng/radiobutton';
+import { SidebarModule } from 'primeng/sidebar';
+import { ChartModule } from 'primeng/chart';
 
 @Component({
   selector: 'poke-dashboard',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    DockModule,
+    RadioButtonModule,
+    CommonModule,
+    FormsModule,
+    SidebarModule,
+    ChartModule,
+  ],
   templateUrl: './poke-dashboard.component.html',
   styleUrl: './poke-dashboard.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -18,4 +30,141 @@ export class PokeDashboardComponent {
   user$: Observable<User | null> = this.store.select(selectUser);
 
   constructor(private store: Store<PokeState>) {}
+
+  lineData: any;
+  lineOptions: any;
+
+  barData: any;
+  basicOptions: any;
+
+  ngOnInit() {
+    const documentStyle = getComputedStyle(document.documentElement);
+    const textColor = documentStyle.getPropertyValue('--text-color');
+    const textColorSecondary = documentStyle.getPropertyValue(
+      '--text-color-secondary'
+    );
+    const surfaceBorder = documentStyle.getPropertyValue('--surface-border');
+
+    this.barData = {
+      labels: ['Q1', 'Q2', 'Q3', 'Q4'],
+      datasets: [
+        {
+          label: 'Sales',
+          data: [540, 325, 702, 620],
+          backgroundColor: [
+            'rgba(255, 159, 64, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(153, 102, 255, 0.2)',
+          ],
+          borderColor: [
+            'rgb(255, 159, 64)',
+            'rgb(75, 192, 192)',
+            'rgb(54, 162, 235)',
+            'rgb(153, 102, 255)',
+          ],
+          borderWidth: 1,
+        },
+      ],
+    };
+
+    this.basicOptions = {
+      plugins: {
+        legend: {
+          labels: {
+            color: textColor,
+          },
+        },
+      },
+      scales: {
+        y: {
+          beginAtZero: true,
+          ticks: {
+            color: textColorSecondary,
+          },
+          grid: {
+            color: surfaceBorder,
+            drawBorder: false,
+          },
+        },
+        x: {
+          ticks: {
+            color: textColorSecondary,
+          },
+          grid: {
+            color: surfaceBorder,
+            drawBorder: false,
+          },
+        },
+      },
+    };
+
+    this.lineData = {
+      labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+      datasets: [
+        {
+          label: 'Dataset 1',
+          fill: false,
+          borderColor: documentStyle.getPropertyValue('--blue-500'),
+          yAxisID: 'y',
+          tension: 0.4,
+          data: [65, 59, 80, 81, 56, 55, 10],
+        },
+        {
+          label: 'Dataset 2',
+          fill: false,
+          borderColor: documentStyle.getPropertyValue('--green-500'),
+          yAxisID: 'y1',
+          tension: 0.4,
+          data: [28, 48, 40, 19, 86, 27, 90],
+        },
+      ],
+    };
+
+    this.lineOptions = {
+      stacked: false,
+      maintainAspectRatio: false,
+      aspectRatio: 0.6,
+      plugins: {
+        legend: {
+          labels: {
+            color: textColor,
+          },
+        },
+      },
+      scales: {
+        x: {
+          ticks: {
+            color: textColorSecondary,
+          },
+          grid: {
+            color: surfaceBorder,
+          },
+        },
+        y: {
+          type: 'linear',
+          display: true,
+          position: 'left',
+          ticks: {
+            color: textColorSecondary,
+          },
+          grid: {
+            color: surfaceBorder,
+          },
+        },
+        y1: {
+          type: 'linear',
+          display: true,
+          position: 'right',
+          ticks: {
+            color: textColorSecondary,
+          },
+          grid: {
+            drawOnChartArea: false,
+            color: surfaceBorder,
+          },
+        },
+      },
+    };
+  }
 }
