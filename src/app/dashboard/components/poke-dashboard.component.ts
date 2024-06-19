e88eb0265b4a0d7d098ae4669d8dc9b9729afe3a
Natalia Veras <verasnatali4@gmail.com>
2024-06-19 20:01:24 +0200

    [Dashboard] Vertical bar: calculate genders per generation

--- src/app/dashboard/components/poke-dashboard.component.ts
+++ src/app/dashboard/components/poke-dashboard.component.ts
@@ -38,16 +38,9 @@ export class PokeDashboardComponent {
   lineOptions: any;
   barData: any;
   basicOptions: any;
-  pokemonTypesData$!: Observable<{
-    labels: string[];
-    datasets: {
-      label: string;
-      data: number[];
-      backgroundColor: string[];
-      borderColor: string[];
-      borderWidth: number;
-    }[];
-  } | null>;
+  pokemonTypesData$!: Observable<ChartData | null>;
+  pokemonGenerationsData$!: Observable<ChartData | null>;
+  pokemonGenderByGenerationsData$!: Observable<ChartData | null>;
 
   constructor(
     private store: Store<PokeState>,
@@ -56,186 +49,12 @@ export class PokeDashboardComponent {
   ) {}
 
   ngOnInit() {
-    this.getPokemonTypesChart();
+    this.getPokemonTypesData();
+    this.getPokemonGenerationsData();
+    this.getPokemonGendersByGenerationData();
   }
 
-  initBarChart() {
-    const documentStyle = getComputedStyle(document.documentElement);
-    const textColor = documentStyle.getPropertyValue('--text-color');
-    const textColorSecondary = documentStyle.getPropertyValue(
-      '--text-color-secondary'
-    );
-    const surfaceBorder = documentStyle.getPropertyValue('--surface-border');
-
-    this.barData = {
-      labels: ['Q1', 'Q2', 'Q3', 'Q4'],
-      datasets: [
-        {
-          label: 'Sales',
-          data: [540, 325, 702, 620],
-          backgroundColor: [
-            'rgba(255, 159, 64, 0.2)',
-            'rgba(75, 192, 192, 0.2)',
-            'rgba(54, 162, 235, 0.2)',
-            'rgba(153, 102, 255, 0.2)',
-          ],
-          borderColor: [
-            'rgb(255, 159, 64)',
-            'rgb(75, 192, 192)',
-            'rgb(54, 162, 235)',
-            'rgb(153, 102, 255)',
-          ],
-          borderWidth: 1,
-        },
-      ],
-    };
-
-    this.basicOptions = {
-      plugins: {
-        legend: {
-          labels: {
-            color: textColor,
-          },
-        },
-      },
-      scales: {
-        y: {
-          beginAtZero: true,
-          ticks: {
-            color: textColorSecondary,
-          },
-          grid: {
-            color: surfaceBorder,
-            drawBorder: false,
-          },
-        },
-        x: {
-          ticks: {
-            color: textColorSecondary,
-          },
-          grid: {
-            color: surfaceBorder,
-            drawBorder: false,
-          },
-        },
-      },
-    };
-  }
-
-  initLineChart() {
-    const documentStyle = getComputedStyle(document.documentElement);
-    const textColor = documentStyle.getPropertyValue('--text-color');
-    const textColorSecondary = documentStyle.getPropertyValue(
-      '--text-color-secondary'
-    );
-    const surfaceBorder = documentStyle.getPropertyValue('--surface-border');
-
-    this.lineData = {
-      labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-      datasets: [
-        {
-          label: 'Dataset 1',
-          fill: false,
-          borderColor: documentStyle.getPropertyValue('--blue-500'),
-          yAxisID: 'y',
-          tension: 0.4,
-          data: [65, 59, 80, 81, 56, 55, 10],
-        },
-        {
-          label: 'Dataset 2',
-          fill: false,
-          borderColor: documentStyle.getPropertyValue('--green-500'),
-          yAxisID: 'y1',
-          tension: 0.4,
-          data: [28, 48, 40, 19, 86, 27, 90],
-        },
-      ],
-    };
-
-    this.lineOptions = {
-      stacked: false,
-      maintainAspectRatio: false,
-      aspectRatio: 0.6,
-      plugins: {
-        legend: {
-          labels: {
-            color: textColor,
-          },
-        },
-      },
-      scales: {
-        x: {
-          ticks: {
-            color: textColorSecondary,
-          },
-          grid: {
-            color: surfaceBorder,
-          },
-        },
-        y: {
-          type: 'linear',
-          display: true,
-          position: 'left',
-          ticks: {
-            color: textColorSecondary,
-          },
-          grid: {
-            color: surfaceBorder,
-          },
-        },
-        y1: {
-          type: 'linear',
-          display: true,
-          position: 'right',
-          ticks: {
-            color: textColorSecondary,
-          },
-          grid: {
-            drawOnChartArea: false,
-            color: surfaceBorder,
-          },
-        },
-      },
-    };
-  }
-
-  getGender() {
-    this.pokeHttpService
-      .get<PokemonData>('/gender', {}, true)
-      .pipe(
-        take(1),
-        map((res) => {
-          console.log(res);
-          return res;
-        }),
-        catchError((error) => {
-          console.log(error);
-          this.toasterService.showErrorMessage('Error getting gender');
-          return of(null);
-        })
-      )
-      .subscribe();
-  }
-
-  getPokemons() {
-    this.pokeHttpService
-      .get<PokemonData>('/pokemon', {}, true)
-      .pipe(
-        take(1),
-        map((res) => {
-          console.log(res);
-          return res;
-        }),
-        catchError((error) => {
-          console.log(error);
-          this.toasterService.showErrorMessage('Error getting gender');
-          return of(null);
-        })
-      )
-      .subscribe();
-  }
-
-  getPokemonTypesChart() {
+  getPokemonTypesData(): void {
     this.pokemonTypesData$ = this.pokeHttpService
       .get<BaseApiResponse>(`${EndPoints.baseUrl}/type`, {}, true)
       .pipe(
@@ -248,8 +67,8 @@ export class PokeDashboardComponent {
                 take(1),
                 map((res) => {
                   return {
-                    pokemons: res.pokemon,
                     name: res.name,
+                    pokemons: res.pokemon,
                   };
                 })
               );
@@ -288,18 +107,281 @@ export class PokeDashboardComponent {
         })
       );
   }
-  upperCaseFirstLetter(name: string): string {
-    return name.slice(0, 1).toLocaleUpperCase() + name.slice(1).toLowerCase();
+
+  getPokemonGendersByGenerationData(): void {
+    this.pokemonGenderByGenerationsData$ = this.pokeHttpService
+      // Retrieve genders
+      .get<BaseApiResponse>(`${EndPoints.baseUrl}/gender`, {}, true)
+      .pipe(
+        take(1),
+        catchError((error) => {
+          console.error('Error retrieving genders:', error);
+          this.toasterService.showErrorMessage('Error retrieving genders');
+          return of(null);
+        }),
+        switchMap((genders) => {
+          if (!genders) {
+            return of(null);
+          }
+          return forkJoin(
+            genders.results.map(({ url }) =>
+              this.pokeHttpService.get<Gender>(url, {}, true).pipe(
+                take(1),
+                map((res) => ({
+                  genderName: res.name,
+                  pokemonSpecies: res.pokemon_species_details,
+                })),
+                catchError((error) => {
+                  console.error(`Error retrieving gender from ${url}:`, error);
+                  this.toasterService.showErrorMessage(
+                    'Error retrieving gender details'
+                  );
+                  return of(null);
+                })
+              )
+            )
+          );
+        }),
+        map((genderDetails) => {
+          if (!genderDetails) {
+            return {
+              genders: [],
+              femalePokemons: null,
+              malePokemons: null,
+              genderLessPokemons: null,
+            };
+          }
+          const gendersName = genderDetails
+            .map((genderDetail) => genderDetail?.genderName)
+            .filter((name) => !!name); // Filter out undefined names
+          const femalePokemons = genderDetails.find(
+            (genderDetail) => genderDetail?.genderName === 'female'
+          );
+          const malePokemons = genderDetails.find(
+            (genderDetail) => genderDetail?.genderName === 'male'
+          );
+          const genderLessPokemons = genderDetails.find(
+            (genderDetail) => genderDetail?.genderName === 'genderless'
+          );
+          return {
+            genders: gendersName,
+            femalePokemons,
+            malePokemons,
+            genderLessPokemons,
+          };
+        }),
+        switchMap((gendersAndPokemons) => {
+          if (!gendersAndPokemons) {
+            return of(null);
+          }
+          return (
+            this.pokeHttpService
+              // Fetch generations
+              .get<BaseApiResponse>(`${EndPoints.baseUrl}/generation`, {}, true)
+              .pipe(
+                take(1),
+                catchError((error) => {
+                  console.error('Error fetching generations:', error);
+                  this.toasterService.showErrorMessage(
+                    'Error fetching generations'
+                  );
+                  return of(null);
+                }),
+                switchMap((res) => {
+                  if (!res) {
+                    return of(null);
+                  }
+                  const responses = res.results.map(({ url }) =>
+                    this.pokeHttpService.get<Generation>(url, {}, true).pipe(
+                      take(1),
+                      map((generationRes) => ({
+                        generation: generationRes.name,
+                        pokemonSpecies: generationRes.pokemon_species,
+                      })),
+                      catchError((error) => {
+                        console.error(
+                          `Error retrieving generation from ${url}:`,
+                          error
+                        );
+                        this.toasterService.showErrorMessage(
+                          'Error retrieving generation details'
+                        );
+                        return of(null);
+                      })
+                    )
+                  );
+                  return forkJoin(responses).pipe(
+                    map((generationResponses) => ({
+                      genders: gendersAndPokemons.genders,
+                      generations: generationResponses
+                        .filter((res) => res)
+                        .map((res) => res?.generation),
+                      genderPokemonNames: {
+                        femalePokemonsNames:
+                          gendersAndPokemons.femalePokemons?.pokemonSpecies.map(
+                            (pokemon) => pokemon.pokemon_species.name
+                          ) || [],
+                        malePokemonsNames:
+                          gendersAndPokemons.malePokemons?.pokemonSpecies.map(
+                            (pokemon) => pokemon.pokemon_species.name
+                          ) || [],
+                        genderlessPokemonsNames:
+                          gendersAndPokemons.genderLessPokemons?.pokemonSpecies.map(
+                            (pokemon) => pokemon.pokemon_species.name
+                          ) || [],
+                      },
+                      generationPokemonNames: generationResponses.map(
+                        (res) => ({
+                          generation: res?.generation,
+                          pokemonNames: res?.pokemonSpecies.map(
+                            (pokemon) => pokemon.name
+                          ),
+                        })
+                      ),
+                    })),
+                    catchError((error) => {
+                      const message = 'Error fetching generation responses:';
+                      console.error(message, error);
+                      this.toasterService.showErrorMessage(message);
+                      return of(null);
+                    })
+                  );
+                })
+              )
+          );
+        }),
+        map((res) => {
+          if (!res) {
+            return null;
+          }
+          const genderPokemonsPerGeneration = res.generationPokemonNames.map(
+            (gen) => ({
+              generation: gen?.generation,
+              genderless: (gen?.pokemonNames || []).filter((name) =>
+                res.genderPokemonNames.genderlessPokemonsNames.includes(name)
+              ),
+              female: (gen?.pokemonNames || []).filter((name) =>
+                res.genderPokemonNames.femalePokemonsNames.includes(name)
+              ),
+              male: (gen?.pokemonNames || []).filter((name) =>
+                res.genderPokemonNames.malePokemonsNames.includes(name)
+              ),
+            })
+          );
+          return {
+            genders: res.genders,
+            generations: res.generations,
+            genderPerGenerations: genderPokemonsPerGeneration,
+          };
+        }),
+        map((chartData) => {
+          if (!chartData || !chartData.genders || !chartData.generations) {
+            return null;
+          }
+          console.log('chartdata 🤠🤠🤠', chartData);
+
+          const backgroundColor = {
+            female: 'hsla(330, 100%, 70%, 1)',
+            male: 'hsla(240, 100%, 50%, 1)',
+            genderless: 'hsla(0, 0%, 50%, 1)',
+          };
+          const dataSetsPerGenders: DataSet[] = chartData.genders?.map(
+            (gender) => ({
+              label: gender || '',
+              data: chartData.genderPerGenerations?.map(
+                (generation) => generation[gender as GenderType]?.length || 0
+              ),
+              backgroundColor: [backgroundColor[gender as GenderType]],
+            })
+          );
+          const labels: string[] = chartData.generations.filter(
+            (gen): gen is string => typeof gen === 'string'
+          );
+          return {
+            labels,
+            datasets: dataSetsPerGenders,
+          };
+        }),
+        catchError((error) => {
+          console.error(
+            'Error retrieving pokémon genders per generation',
+            error
+          );
+          this.toasterService.showErrorMessage(
+            'Error retrieving pokémon genders per generation'
+          );
+          return of(null);
+        })
+      );
   }
 
-  // Generations -> bar chart
+  getPokemonGenerationsData(): void {
+    this.pokemonGenerationsData$ = this.pokeHttpService
+      .get<BaseApiResponse>(`${EndPoints.baseUrl}/generation`, {}, true)
+      .pipe(
+        take(1),
+        switchMap((res) => {
+          const responses = res.results.map(({ url }) => {
+            return this.pokeHttpService.get<Generation>(url, {}, true).pipe(
+              take(1),
+              map((res) => {
+                return {
+                  name: res.name,
+                  pokemons: res.pokemon_species,
+                };
+              })
+            );
+          });
+          return forkJoin(responses);
+        }),
+        map((res) => {
+          const sortRes = res
+            .filter(({ pokemons }) => pokemons.length)
+            .sort((a, b) => a.pokemons.length - b.pokemons.length);
+          const data = sortRes.map((data) => data.pokemons.length);
+          const labels = sortRes.map((data) =>
+            this.formatGenerationString(data.name)
+          );
+          const colors = this.generateColors(data.length);
+
+          return {
+            labels,
+            datasets: [
+              {
+                label: 'Pokémons of this generation',
+                data,
+                backgroundColor: colors.backgroundColor,
+                borderColor: colors.borderColor,
+                borderWidth: 2,
+              },
+            ],
+          };
+        }),
+        catchError((error) => {
+          console.error('Error retrieving pokémon generations', error);
+          this.toasterService.showErrorMessage(
+            'Error retrieving pokémon generations'
+          );
+          return of(null);
+        })
+      );
+  }
+
   // Genders -> pie chart
   // Gender by generations -> Vertical Bar
   // catch rate capture_rate:190 -> Higher catch rates mean that the Pokémon is easier to catch, up to a maximum of 255.
   // /pokemon-species/1 base_happiness:50
   // capture_rate:45
 
-  private generateColors(count: number): {
+  formatGenerationString(input: string): string {
+    const parts = input.split('-');
+    const firstPart =
+      parts[0].charAt(0).toUpperCase() + parts[0].slice(1).toLowerCase();
+    const secondPart = parts[1].toUpperCase();
+    return `${firstPart}-${secondPart}`;
+  }
+
+  generateColors(count: number): {
     backgroundColor: string[];
     borderColor: string[];
   } {
@@ -314,27 +396,10 @@ export class PokeDashboardComponent {
 
     return { backgroundColor, borderColor };
   }
-}
 
-enum TYPES {
-  NORMAL = '1',
-  FIGHTING = '2',
-  FLYING = '3',
-  POISON = '4',
-  GROUND = '5',
-  ROCK = '6',
-  BUG = '7',
-  GHOST = '8',
-  STEEL = '9',
-  FIRE = '10',
-  WATER = '11',
-  GRASS = '12',
-  ELECTRIC = '13',
-  PSYCHIC = '14',
-  ICE = '15',
-  DRAGON = '16',
-  DARK = '17',
-  FAIRY = '18',
+  upperCaseFirstLetter(name: string): string {
+    return name.slice(0, 1).toLocaleUpperCase() + name.slice(1).toLowerCase();
+  }
 }
 
 interface PokemonSpecies {
@@ -357,4 +422,93 @@ interface PokemonData {
   name: string;
   pokemon_species_details: PokemonSpeciesDetails[];
   required_for_evolution: RequiredForEvolution[];
+}
+
+interface ChartData {
+  labels: string[];
+  datasets: DataSet[];
+}
+
+interface DataSet {
+  label: string | undefined;
+  data: number[];
+  backgroundColor: string[];
+  borderColor?: string[];
+  borderWidth?: number;
+}
+
+// Generations
+interface Language {
+  name: string;
+  url: string;
+}
+
+interface NamedAPIResource {
+  name: string;
+  url: string;
+}
+
+interface Generation {
+  abilities: any[];
+  id: number;
+  main_region: NamedAPIResource;
+  moves: NamedAPIResource[];
+  name: string;
+  names: {
+    language: NamedAPIResource;
+    name: string;
+  }[];
+  pokemon_species: NamedAPIResource[];
+}
+
+// Genders by generation
+interface PokemonSpeciesDetails {
+  pokemon_species: {
+    name: string;
+    url: string;
+  };
+  rate: number;
+}
+
+interface Gender {
+  id: number;
+  name: string;
+  pokemon_species_details: PokemonSpeciesDetails[];
+}
+
+type GenderType = 'genderless' | 'female' | 'male';
+
+interface Specie {
+  generation: {
+    name: string;
+    url: string;
+  };
+  base_happiness: number;
+  capture_rate: number;
+  color: {
+    name: string;
+    url: string;
+  };
+  egg_groups: {
+    name: string;
+    url: string;
+  }[];
+  evolution_chain: {
+    url: string;
+  };
+  evolves_from_species: null | {
+    name: string;
+    url: string;
+  };
+  flavor_text_entries: {
+    flavor_text: string;
+    language: {
+      name: string;
+      url: string;
+    };
+    version: {
+      name: string;
+      url: string;
+    };
+  }[];
 }
